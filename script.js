772c3158de2cce00ac5bac3f4408542273399c09
verloxs <gtsbyxx@gmail.com>
2022-04-10 05:07:12 -0400

    cleaned up js, added toggle to  help reset text

--- script.js
+++ script.js
@@ -1,11 +1,4 @@
-let screenValue = "";
-
 const input = document.getElementById("name"); //text box/display element
-
-// updates value if user manually inputs
-input.addEventListener("input", function () {
-  screenValue = input.value;
-});
 
 let stored = ""; //store number
 let operand = ""; // store operator
@@ -29,27 +22,31 @@ let divide = (x, y) => {
   return x / y;
 };
 
+let toggle = true; // toggle false in order to let numberListener know to wipe text on subsequent input, then toggle true
+
 // updates value if user presses on screen button
 for (var i = 0; i < numbers.length; i++) {
   numbers[i].addEventListener("click", function () {
-    if (stored != "" && input.value == "") {
+    if (toggle == false) {
+      // if toggle false, reset text on next button press
       input.value = "";
+      toggle = true;
     }
     input.value += this.value;
-    screenValue = input.value;
   });
 }
 
 for (var i = 0; i < operators.length; i++) {
   operators[i].addEventListener("click", function () {
+    // if there is already an operator
     if (operand != "") {
-      // if operand exists
-      opera(stored, screenValue, operand);
-      operand = this.value;
+      toggle = false;
+      opera(stored, input.value, operand); // evaluate the operator
+      operand = this.value; // store the pressed operator
     } else {
-      stored = screenValue;
+      toggle = false;
+      stored = input.value;
       operand = this.value;
-      input.value = "";
     }
   });
 }
@@ -57,31 +54,59 @@ for (var i = 0; i < operators.length; i++) {
 //12 + 7 - 5 * 3 = should yield 42.
 
 let opera = (storedNumber, currentNumber, op) => {
-  let storedInt = parseInt(storedNumber);
-  let currentInt = parseInt(currentNumber);
+  if (storedNumber.includes(".") || currentNumber.includes(".")) {
+    // if dealing with decimals
+    let storedInt = parseFloat(storedNumber);
+    let currentInt = parseFloat(currentNumber);
 
-  if (op == "+") {
-    input.value = add(storedInt, currentInt);
-    stored = input.value;
-    operand = "";
-  } else if (op == "-") {
-    input.value = subtract(storedInt, currentInt);
-    stored = input.value;
-    operand = "";
-  } else if (op == "*") {
-    input.value = multiply(storedInt, currentInt);
-    stored = input.value;
-    operand = "";
-  } else if (op == "/") {
-    input.value = divide(storedInt, currentInt);
-    stored = input.value;
-    operand = "";
+    if (op == "+") {
+      input.value = add(storedInt, currentInt);
+      stored = input.value;
+      operand = "";
+    } else if (op == "-") {
+      input.value = subtract(storedInt, currentInt);
+      stored = input.value;
+      operand = "";
+    } else if (op == "*") {
+      input.value = multiply(storedInt, currentInt);
+      stored = input.value;
+      operand = "";
+    } else if (op == "/") {
+      input.value = divide(storedInt, currentInt);
+      stored = input.value;
+      operand = "";
+    }
+  } else {
+    // if not deicmals
+    let storedInt = parseInt(storedNumber);
+    let currentInt = parseInt(currentNumber);
+
+    if (op == "+") {
+      input.value = add(storedInt, currentInt);
+      stored = input.value;
+      operand = "";
+    } else if (op == "-") {
+      input.value = subtract(storedInt, currentInt);
+      stored = input.value;
+      operand = "";
+    } else if (op == "*") {
+      input.value = multiply(storedInt, currentInt);
+      stored = input.value;
+      operand = "";
+    } else if (op == "/") {
+      input.value = divide(storedInt, currentInt);
+      stored = input.value;
+      operand = "";
+    }
   }
 };
 
 let wipe = () => {
+  //clear calculator
   stored = "";
   operand = "";
-  screenValue = "";
   input.value = "";
+  toggle = true;
 };
+
+let equalToggle = () => (toggle = false);
